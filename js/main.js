37db6b778b1bde2300b6c726c0b8a543822a77c1
shadowruge <zambelestar@gmail.com>
2022-05-13 19:20:28 -0300

    create leading page

---
+++ js/main.js
@@ -0,0 +1,109 @@
+let personagens = [];
+
+personagens.mago = {
+    id:0,
+    força: 10,
+    destreza: 10,
+    inteligência: 20,
+    hp: 100,
+    mp: 100,
+    ataque: 10,
+    defesa: 10,
+    nome: "",
+    descricao: "Mago é um personagem que tem a capacidade de conjurar magias",
+    imagem: "/imagens/mago.png"
+
+};
+
+personagens.guerreiro = {
+    id:1,
+    força: 10,
+    destreza: 10,
+    inteligência: 20,
+    hp: 100,
+    mp: 100,
+    ataque: 10,
+    defesa: 10,
+    nome: "",
+    descricao: "Guereiro é um personagem que tem força e abilidades destrutivas",
+    imagem: "/imagens/guerreiro.png"
+
+};
+
+personagens.ladino = {
+    id:2,
+    força: 10,
+    destreza: 10,
+    inteligência: 20,
+    hp: 100,
+    mp: 100,
+    ataque: 10,
+    defesa: 10,
+    nome: "",
+    descricao: "Ladino ladrão sorrateiro e perigoso",
+    imagem: "/imagens/ladino.png" 
+
+};
+
+personagens.paladino = {
+    id:3,
+    força: 10,
+    destreza: 10,
+    inteligência: 20,
+    hp: 100,
+    mp: 100,
+    ataque: 10,
+    defesa: 10,
+    nome: "",
+    descricao: "Defensor da ordem, personagem que tem força e destreza",
+    imagem:"/imagens/paladino.png"
+
+};
+
+personagens.bruxo = {
+    id:4,
+    força: 10,
+    destreza: 10,
+    inteligência: 20,
+    hp: 100,
+    mp: 100,
+    ataque: 10,
+    defesa: 10,
+    nome: "",
+    descricao: "Bruxo a invocação do mal",
+    imagem: "/imagens/bruxo.png" 
+
+};
+
+personagens.druida = {
+    id:5,
+    força: 10,
+    destreza: 10,
+    inteligência: 20,
+    hp: 100,
+    mp: 100,
+    ataque: 10,
+    defesa: 10,
+    nome: "",
+    descricao: "Druida protetor da natureza, tem a força dos animais",
+    imagem: "/imagens/druida.png"
+
+};
+
+personagens.elfa = {
+    id:6,
+    força: 10,
+    destreza: 10,
+    inteligência: 20,
+    hp: 100,
+    mp: 100,
+    ataque: 10,
+    defesa: 10,
+    nome: "",
+    descricao: "Druida protetor da natureza, tem a força dos animais",
+    imagem: "/imagens/elfa.png"
+
+};
+
+
+console.log(personagens);
